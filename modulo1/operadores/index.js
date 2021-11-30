aa7c7230ac2ffbd6fb33de108782125cf80d4d98
mjfneto <mjfneto@gmail.com>
2021-11-30 14:31:30 -0300

    Soluçoes para os exercicios de escrita de codigo

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -37,3 +37,44 @@ console.log(soma)
 */ 
 
 /* Exercícios de escrita de código */
+
+// Exercício 1
+
+const age = Number(prompt('Quantos anos você tem?'));
+const bFAge = Number(prompt('Quantos anos tem seu melhor amigo?'));
+
+console.log(`Sua idade é maior do que a do seu melhor amigo? ${age > bFAge}`);
+console.log(age - bFAge);
+
+// Exercício 2
+
+const even = Number(prompt('Insira um número par.'));
+
+console.log(even % 2);
+
+/** 
+ * O retorno dos testes com os outros números pares 
+ * também foi 0, ou seja, são divisíveis por 2. Logo, 
+ * números ímpares retornarão um valor diferente de 0.
+*/
+
+// Exercício 3
+
+const userAge = Number(prompt('Qual é a sua idade?'));
+const userAgeMonths = userAge * 12;
+const userAgeDays = userAge * 365;
+const userAgeHours = userAgeDays * 24;
+
+console.log(userAgeMonths + ' meses');
+console.log(userAgeDays + ' dias');
+console.log(userAgeHours + ' horas');
+
+// Exercício 4
+
+const num1 = Number(prompt('Digite um número'));
+const num2 = Number(prompt('Agora, outro número.'));
+
+console.log(`O primeiro número é maior que o segundo? ${num1 > num2}`);
+console.log(`O primeiro número é igual ao segundo? ${num1 == num2}`);
+console.log(`O primeiro número é divisível pelo segundo? ${num1 % num2 == 0}`);
+console.log(`O segundo número é divisível pelo primeiro? ${num2 % num1 == 0}`);
