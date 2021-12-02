55043f64a3a5a170225144c74b5cd4c8435fb292
mjfneto <mjfneto@gmail.com>
2021-12-02 14:06:46 -0300

    Resoluçoes dos exercicios de escrita de codigo

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -41,3 +41,65 @@ const outraFuncao = function (texto) {
 
 const resposta = outraFuncao(textoDoUsuario);
 console.log(resposta);
+
+/* Exercícios de escrita de código */
+
+// Exercício 1
+
+function myInfo() {
+  console.log("Eu sou Mario, tenho 30 anos, moro em Resende e sou estudante.");
+}
+
+function makeInfo(nome, idade, cidade, profissao) {
+  return `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`;
+}
+
+console.log(makeInfo("Laís", 23, "São Paulo", "instrutora"));
+
+// Exercício 2
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function greaterThan(num1, num2) {
+  return num1 >= num2;
+}
+
+function isEven(num) {
+  return num % 2 == 0;
+}
+
+function lengthUpperCase(mensagem) {
+  console.log(mensagem.length, mensagem.toUpperCase());
+}
+
+console.log(sum(11, 13));
+console.log(greaterThan(11, 13));
+console.log(isEven(13));
+console.log(lengthUpperCase("Essa string contém 33 caracteres."));
+
+// Exercício 3
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+const numX = Number(prompt("Insira um número"));
+const numY = Number(prompt("Insira outro número"));
+
+console.log(`
+    Números inseridos: ${numX} e ${numY}
+    Soma: ${sum(numX, numY)}
+    Diferença: ${subtract(numX, numY)}
+    Multiplicação: ${multiply(numX, numY)}
+    Divisão: ${divide(numX, numY)}
+`);
